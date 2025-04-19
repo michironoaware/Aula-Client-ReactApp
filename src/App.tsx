e45af85c74b54e40b64809d0e89848537c8b21b3
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-19 08:12:03 -0300

    apply overlays

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Body from "./Body";
 export default function App()
 {
 	return <div className="app">
+		<div className="radial"></div>
+		<div className="flicker"></div>
+		<div className="scanline"></div>
+		<div className="scanlines"></div>
 		<Body></Body>
 	</div>;
 }

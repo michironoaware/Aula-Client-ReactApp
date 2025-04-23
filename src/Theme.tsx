ae2900805d8c766b861fff20e2c29e9892434f3a
rexaliis <77251557+rexaliis@users.noreply.github.com>
2025-04-23 02:00:53 -0300

    Create Theme.tsx

---
+++ src/Theme.tsx
@@ -0,0 +1,26 @@
+﻿import React from "react";
+
+export default function Theme(args: ThemeArgs)
+{
+	return <div className={`theme ${args.theme}`}>
+		<div className="radial"></div>
+		<div className="flicker"></div>
+		<div className="scanline"></div>
+		<div className="scanlines"></div>
+		{args.children}
+	</div>
+}
+
+export enum ThemeType
+{
+	Dark = "dark-theme",
+	Light = "light-theme",
+	Onyx = "onyx-theme",
+	Matrix = "matrix-theme",
+}
+
+export interface ThemeArgs
+{
+	children: React.ReactNode;
+	theme: ThemeType;
+}

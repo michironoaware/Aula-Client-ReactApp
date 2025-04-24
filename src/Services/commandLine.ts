8992585f95a05baa38526f6dd7a16fbe6983a4bd
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 15:01:05 -0300

    add SetTheme command to command line

--- src/Services/commandLine.ts
+++ src/Services/commandLine.ts
@@ -2,7 +2,9 @@
 import { loggers } from "./loggers.ts";
 import { SetAddress } from "./Commands/SetAddress.ts";
 import { Help } from "./Commands/Help.ts";
+import { SetTheme } from "./Commands/SetTheme.ts";
 
 export const commandLine = new CommandLine(loggers);
 commandLine.addCommand(new Help());
+commandLine.addCommand(new SetTheme());
 commandLine.addCommand(new SetAddress());

6c2ad12b57c55d5a76c0efbfbf0ffac7d239c33a
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 02:05:41 -0300

    modify App component contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Body from "./Body";
 import Theme, { ThemeType } from "./Theme.tsx";
+import InputTextBox from "./InputTextBox.tsx";
+
 const exampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum;"
 
 export default function App()
 {
 	return <Theme theme={ThemeType.Dark}>
-		<Body></Body>
+		<Body>
+			{exampleText}
+			<InputTextBox></InputTextBox>
+		</Body>
 	</Theme>
 }

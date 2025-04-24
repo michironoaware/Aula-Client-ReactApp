bbaa20e324aa702036a56d1998d356586d8c5918
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 13:37:18 -0300

    add Help command to the command line

--- src/Services/commandLine.ts
+++ src/Services/commandLine.ts
@@ -1,6 +1,8 @@
 ﻿import { CommandLine } from "../Commands";
 import { loggers } from "./loggers.ts";
 import { SetAddress } from "./Commands/SetAddress.ts";
+import { Help } from "./Commands/Help.ts";
 
 export const commandLine = new CommandLine(loggers);
+commandLine.addCommand(new Help());
 commandLine.addCommand(new SetAddress());

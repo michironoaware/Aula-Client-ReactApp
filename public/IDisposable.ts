e3e3f236bf9eda45209435bb70badc2d0b719266
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 02:35:55 -0300

    Create IDisposable.ts

---
+++ public/IDisposable.ts
@@ -0,0 +1,10 @@
+﻿/**
+ * Provides a mechanism for releasing unmanaged resources.
+ * */
+export interface IDisposable
+{
+	/**
+	 * Performs application-defined tasks associated with freeing, releasing, or resetting resources.
+	 */
+	[Symbol.dispose](): void;
+}

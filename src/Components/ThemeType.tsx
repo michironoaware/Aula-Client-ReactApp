174bba8eb49385b56cf0aeabd1bf60cbff0527c1
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 15:00:44 -0300

    reset ThemeType values

--- src/Components/ThemeType.tsx
+++ src/Components/ThemeType.tsx
@@ -1,7 +1,21 @@
-﻿export enum ThemeType
+﻿import { InvalidOperationError } from "aula.js";
+
+export enum ThemeType
 {
-	Vintage = "vintage-theme",
-	Light = "light-theme",
-	Onyx = "onyx-theme",
-	Matrix = "matrix-theme",
+	Vintage = 0,
+	Light = 1,
+	Onyx = 2,
+	Matrix = 3,
+}
+
+export function ThemeTypeToDomClass(theme: ThemeType)
+{
+	switch (theme)
+	{
+		case ThemeType.Vintage: return "vintage-theme";
+		case ThemeType.Light: return "light-theme";
+		case ThemeType.Onyx: return "onyx-theme";
+		case ThemeType.Matrix: return "matrix-theme";
+		default: throw new InvalidOperationError(`Unknown theme type: ${theme}`);
+	}
 }

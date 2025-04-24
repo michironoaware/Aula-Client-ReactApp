f8dbfce5c6e1573b32f68e23ee4b64efcf712edf
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 04:34:05 -0300

    Create StringBuilder.ts

---
+++ src/Common/StringBuilder.ts
@@ -0,0 +1,27 @@
+﻿import { TypeHelper } from "./TypeHelper.ts";
+
+export class StringBuilder
+{
+	#_strings: string | string[];
+
+	public constructor(string: string = "")
+	{
+		this.#_strings = string;
+	}
+
+	public add(string: string)
+	{
+		if (typeof this.#_strings === "string")
+		{
+			this.#_strings = [ this.#_strings, string ];
+			return;
+		}
+
+		this.#_strings.push(string);
+	}
+
+	public toString()
+	{
+		return typeof this.#_strings === "string" ? this.#_strings : this.#_strings.join();
+	}
+}

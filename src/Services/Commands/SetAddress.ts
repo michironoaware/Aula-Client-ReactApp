99fba1ba6a3d4bcaaa9dc9421e0ce7039ce7db29
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 11:39:20 -0300

    Create SetAddress.ts

---
+++ src/Services/Commands/SetAddress.ts
@@ -0,0 +1,63 @@
+﻿import { Command, CommandOption } from "../../Commands";
+import { CancellationToken } from "aula.js";
+import { aulaClient } from "../aulaClient.ts";
+import { loggers } from "..";
+import { LogLevel } from "../../Common/Logging";
+
+export class SetAddress extends Command
+{
+	static readonly #s_addressOption = new CommandOption({
+		name: "set-address",
+		description: "The address of the server.",
+		isRequired: true,
+		requiresArgument: true,
+		canOverflow: false,
+	});
+
+	public constructor()
+	{
+		super();
+		this.addOption(SetAddress.#s_addressOption);
+	}
+
+	public get name()
+	{
+		return "set-address"
+	}
+
+	public get description()
+	{
+		return "Sets the address of the Aula server."
+	}
+
+	public async callback(args: Readonly<Map<string, string>>, cancellationToken: CancellationToken)
+	{
+		const addressString = args.get(SetAddress.#s_addressOption.name)!;
+
+		let address;
+		try
+		{
+			address = new URL(addressString);
+		}
+		catch (err)
+		{
+			if (!(err instanceof TypeError))
+				// Unexpected error, rethrow.
+				throw err;
+
+			loggers.log(LogLevel.Error, "The address is not a valid URI.");
+			return;
+		}
+
+		loggers.log(LogLevel.Information, "Checking in with the server...");
+		if (!await aulaClient.rest.ping(address))
+		{
+			loggers.log(LogLevel.Error, "The server did not respond. Check if the address is correct or if the server is connected.");
+			return;
+		}
+
+		aulaClient.rest.withAddress(address);
+		loggers.log(LogLevel.Information, "Address updated!");
+		return;
+	}
+}

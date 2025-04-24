4e8b4470eef2a5ffaf4730105a4eeec2c449131f
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 11:38:42 -0300

    make LogList show logs

--- src/Components/LogList.tsx
+++ src/Components/LogList.tsx
@@ -1,13 +1,38 @@
-﻿import { ReactNode } from "react";
+﻿import { ReactNode, useEffect, useState } from "react";
+import { loggers } from "../Services";
+import { ILogger, LogLevel } from "../Common/Logging";
+import Log from "./Log.tsx";
 
 export default function LogList(args: LogListArgs)
 {
+	const [logs, setLogs] = useState<LogData[]>([]);
+
+	useEffect(() =>
+	{
+		const logger = {
+			log: (level, message) =>
+			{
+				setLogs([...logs, { level, message }]);
+			}
+		} satisfies ILogger;
+		loggers.add(logger);
+
+		return () => loggers.remove(logger);
+	}, [logs, setLogs]);
+
 	return <div className="loglist">
 		{args.children}
+		{logs.map(l => <Log logLevel={l.level} message={l.message}/>)}
 	</div>
 }
 
 export interface LogListArgs
 {
 	children?: ReactNode
+}
+
+interface LogData
+{
+	level: LogLevel;
+	message: string;
 }

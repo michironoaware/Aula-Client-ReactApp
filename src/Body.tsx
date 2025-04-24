fae3c8fdbe25329f2f387f7d751e3dac13a8afbc
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 02:05:17 -0300

    Enable passing childrens to Body

--- src/Body.tsx
+++ src/Body.tsx
@@ -1,8 +1,12 @@
 ﻿import React from "react";
 
 const exampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum;"
-
-export default function App()
+export interface BodyArgs
 {
-	return <div className="body text-shadow">{exampleText}</div>
+	children?: React.ReactNode;
+}
+
+export default function Body(args: BodyArgs)
+{
+	return <div className="body text-shadow">{args.children}</div>
 }

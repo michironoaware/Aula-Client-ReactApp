22696b8bed702a27220b4d2b430e53a8e7c52400
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 17:16:18 -0300

    revert "generate key for log data"

--- src/Components/LogList.tsx
+++ src/Components/LogList.tsx
@@ -12,7 +12,7 @@ export default function LogList(args: LogListArgs)
 		const logger = {
 			log: (level, message) =>
 			{
-				setLogs(previousLogs => [...previousLogs, { level, message, generatedKey: crypto.randomUUID() }]);
+				setLogs(previousLogs => [...previousLogs, { level, message }]);
 			}
 		} satisfies ILogger;
 		loggers.add(logger);
@@ -22,7 +22,7 @@ export default function LogList(args: LogListArgs)
 
 	return <div className="loglist">
 		{args.children}
-		{logs.map(l => <Log key={l.generatedKey} logLevel={l.level} message={l.message}/>)}
+		{logs.map(l => <Log logLevel={l.level} message={l.message}/>)}
 	</div>
 }
 
@@ -33,7 +33,6 @@ export interface LogListArgs
 
 interface LogData
 {
-	generatedKey: string;
 	level: LogLevel;
 	message: string;
 }

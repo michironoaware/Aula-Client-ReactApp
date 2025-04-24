780b2d450df38bd64f1c37cd8fd20e552ab3b0ee
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 09:04:20 -0300

    add LogList to App's Body

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -3,6 +3,7 @@ import Body from "./Body";
 import Theme from "./Theme.tsx";
 import InputTextBox from "./InputTextBox.tsx";
 import { TypeHelper } from "../Common";
+import LogList from "./LogList.tsx";
 import { ThemeType } from "./ThemeType.tsx";
 
 const exampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum;"
@@ -19,6 +20,7 @@ export default function App()
 	return <Theme theme={theme} withEffects={true}>
 		<Body>
 			{exampleText}
+			<LogList></LogList>
 			<InputTextBox></InputTextBox>
 		</Body>
 	</Theme>

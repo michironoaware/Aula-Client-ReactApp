54869483342a8692b91c9400a33bfbb2e4e6a263
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 11:20:47 -0300

    process inputtextbox inputs

--- src/Components/InputTextBox.tsx
+++ src/Components/InputTextBox.tsx
@@ -1,20 +1,47 @@
 ﻿import React, { useId } from "react";
+import { CancellationTokenSource, OperationCanceledError } from "aula.js";
+import { commandLine, loggers } from "../Services";
+import { LogLevel } from "../Common/Logging";
 
-export default function InputTextBox(args: InputTextBoxArgs)
+export default function InputTextBox()
 {
 	const textareaId = useId();
+	const onKeyDown: React.KeyboardEventHandler<HTMLTextAreaElement> = async (event) =>
+	{
+		if (event.key !== "Enter")
+			return;
 
+		event.preventDefault();
+
+		const inputElement = document.getElementById(textareaId) as HTMLTextAreaElement;
+		const content = inputElement.value;
+		inputElement.value = "";
+
+		const cancellation = new CancellationTokenSource();
+		const timeout = setTimeout(cancellation.cancel, 5 * 1000);
+
+		try
+		{
+			await commandLine.processCommand(content, cancellation.token);
+		}
+		catch (err)
+		{
+			if (!(err instanceof OperationCanceledError))
+				// Unexpected error, rethrow.
+				throw err;
+
+			loggers.log(LogLevel.Error, "The operation has timed out.");
+		}
+
+		clearTimeout(timeout);
+	}
 	return <div className="input-text-box">
 		<label htmlFor={textareaId}>{">"}</label>
 		<textarea
 			id={textareaId}
 			autoFocus={true}
-			onKeyDown={args.onKeyDown}>
+			onKeyDown={onKeyDown}>
 		</textarea>
 	</div>
 }
 
-export interface InputTextBoxArgs
-{
-	onKeyDown: React.KeyboardEventHandler<HTMLTextAreaElement> | undefined
-}

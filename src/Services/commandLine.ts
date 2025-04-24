d427b7252b6c2f40a5c335a8825072fef949db8e
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 11:39:22 -0300

    Update commandLine.ts

--- src/Services/commandLine.ts
+++ src/Services/commandLine.ts
@@ -1,4 +1,6 @@
 ﻿import { CommandLine } from "../Commands";
 import { loggers } from "./loggers.ts";
+import { SetAddress } from "./Commands/SetAddress.ts";
 
 export const commandLine = new CommandLine(loggers);
+commandLine.addCommand(new SetAddress());

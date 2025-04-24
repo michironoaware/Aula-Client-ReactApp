64528223286a97af956fe632dca453fbb7ec5e9e
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 03:42:11 -0300

    reorganize src

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import Body from "./Body";
 import Theme, { ThemeType } from "./Theme.tsx";
 import InputTextBox from "./InputTextBox.tsx";
+import { TypeHelper } from "../Common";
 
 const exampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum;"
 
 export default function App()
 {
-	return <Theme theme={ThemeType.Dark}>
+	let theme = localStorage.getItem("theme") as ThemeType;
+	if (theme === null || !TypeHelper.isType(theme, ThemeType))
+	{
+		theme = ThemeType.Dark;
+		localStorage.setItem("theme", theme);
+	}
+
+	return <Theme theme={theme}>
 		<Body>
 			{exampleText}
 			<InputTextBox></InputTextBox>

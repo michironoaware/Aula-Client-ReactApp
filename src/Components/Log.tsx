cf6b1509116291c8723a8b2fe9c209f98c8f5007
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 05:01:25 -0300

    Create Log.tsx

---
+++ src/Components/Log.tsx
@@ -0,0 +1,27 @@
+﻿import { LogLevel } from "../Common/Logging";
+import { InvalidOperationError } from "aula.js-alpha";
+
+export default function Log(args: LogArgs)
+{
+	let logLevelClass: string;
+	switch (args.logLevel)
+	{
+		case LogLevel.Critical: logLevelClass = "loglevel-critical"; break;
+		case LogLevel.Error: logLevelClass = "loglevel-error"; break;
+		case LogLevel.Warning: logLevelClass = "loglevel-warning"; break;
+		case LogLevel.Information: logLevelClass = "loglevel-information"; break;
+		case LogLevel.Debug: logLevelClass = "loglevel-debug"; break;
+		case LogLevel.Trace: logLevelClass = "loglevel-trace"; break;
+		default: throw new InvalidOperationError(`Unexpected log level "${args.logLevel}"`);
+	}
+
+	return <div className={logLevelClass}>
+		<pre>{args.message}</pre>
+	</div>
+}
+
+export interface LogArgs
+{
+	logLevel: LogLevel;
+	message: string;
+}

18f49217d59623c6f63f80ad33cf94c30ce9077b
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 16:01:09 -0300

    add logger to dev console

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./Components/App.tsx";
+import { loggers } from "./Services";
+import { LogLevel } from "./Common/Logging";
+
+loggers.add({
+	log: (logLevel, message) => console.log(`[${LogLevel[logLevel]}]: ${message}`),
+});
 
 const root = createRoot(document.getElementById("root")!);
 

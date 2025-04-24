5ed6d21b0c1e09d88325f7f5641fdacf0a40d832
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-24 16:41:37 -0300

    load server address from local storage

--- src/Services/aulaClient.ts
+++ src/Services/aulaClient.ts
@@ -1,3 +1,12 @@
 ﻿import { GatewayClient } from "aula.js";
+import { ErrorHelper } from "../Common";
 
 export const aulaClient = new GatewayClient();
+
+const localStorageAddressName = "server-address";
+const storedAddress = localStorage.getItem(localStorageAddressName);
+if (storedAddress !== null &&
+    ErrorHelper.Try(() => new URL(storedAddress)))
+{
+	aulaClient.withAddress(new URL(storedAddress));
+}

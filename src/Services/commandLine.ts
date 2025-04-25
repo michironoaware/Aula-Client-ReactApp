2a86f9665c2c45db3d79ad1ae938f9d48a1a2e19
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-25 05:56:25 -0300

    add login command to command line

--- src/Services/commandLine.ts
+++ src/Services/commandLine.ts
@@ -3,8 +3,10 @@ import { loggers } from "./loggers.ts";
 import { SetAddress } from "./Commands/SetAddress.ts";
 import { Help } from "./Commands/Help.ts";
 import { SetTheme } from "./Commands/SetTheme.ts";
+import { LogIn } from "./Commands/LogIn.ts";
 
 export const commandLine = new CommandLine(loggers);
 commandLine.addCommand(new Help());
 commandLine.addCommand(new SetTheme());
 commandLine.addCommand(new SetAddress());
+commandLine.addCommand(new LogIn());

b3ec9516a2a797f91c7a1f24f557083e3ef6faad
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-26 03:10:37 -0300

    add set-loglevel command to commandline

--- src/Services/commandLine.ts
+++ src/Services/commandLine.ts
@@ -5,10 +5,12 @@ import { Help } from "./Commands/Help.ts";
 import { SetTheme } from "./Commands/SetTheme.ts";
 import { LogIn } from "./Commands/LogIn.ts";
 import { Cls } from "./Commands/Cls.ts";
+import { SetLogLevel } from "./Commands/SetLogLevel.ts";
 
 export const commandLine = new CommandLine(loggers);
 commandLine.addCommand(new Help());
 commandLine.addCommand(new SetTheme());
 commandLine.addCommand(new SetAddress());
+commandLine.addCommand(new SetLogLevel());
 commandLine.addCommand(new Cls());
 commandLine.addCommand(new LogIn());

260b5f6ed96eec4c7137620dd73a137e2aaff3ab
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-26 02:33:36 -0300

    add Cls command to commandline

--- src/Services/commandLine.ts
+++ src/Services/commandLine.ts
@@ -4,9 +4,11 @@ import { SetAddress } from "./Commands/SetAddress.ts";
 import { Help } from "./Commands/Help.ts";
 import { SetTheme } from "./Commands/SetTheme.ts";
 import { LogIn } from "./Commands/LogIn.ts";
+import { Cls } from "./Commands/Cls.ts";
 
 export const commandLine = new CommandLine(loggers);
 commandLine.addCommand(new Help());
 commandLine.addCommand(new SetTheme());
 commandLine.addCommand(new SetAddress());
+commandLine.addCommand(new Cls());
 commandLine.addCommand(new LogIn());

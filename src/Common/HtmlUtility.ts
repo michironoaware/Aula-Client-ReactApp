e555eda7e8c1f1f9f5100e6d82c73f553dc10041
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-27 05:28:33 -0300

    Create HtmlUtility.ts

---
+++ src/Common/HtmlUtility.ts
@@ -0,0 +1,21 @@
+﻿export namespace HtmlUtility
+{
+	export function escapeHtml(text: string)
+	{
+		return text.replace(/&/g, "&amp;")
+		           .replace(/</g, "&lt;")
+		           .replace(/>/g, "&gt;")
+		           .replace(/"/g, "&quot;")
+		           .replace(/'/g, "&#39;");
+	}
+
+	export function getHtmlFromMarkdown(text: string)
+	{
+		return text
+			.replace(/~~(.+?)~~/g, "<del>$1</del>")
+			.replace(/\*\*(.+?)\*\*/g, "<strong>$1</strong>")
+			.replace(/__(.+?)__/g, "<strong>$1</strong>")
+			.replace(/\*(.+?)\*/g, "<em>$1</em>")
+			.replace(/_(.+?)_/g, "<em>$1</em>");
+	}
+}

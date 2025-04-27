175923e4ab85b09c74b47d6695abb0878c5f91bf
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-27 04:12:47 -0300

    combine into single switch statement

--- src/Components/Log.tsx
+++ src/Components/Log.tsx
@@ -4,28 +4,33 @@ import { InvalidOperationError } from "aula.js";
 export default function Log(args: LogArgs)
 {
 	let logLevelClass: string;
-	switch (args.logLevel)
-	{
-		case LogLevel.Critical: logLevelClass = "loglevel-critical"; break;
-		case LogLevel.Error: logLevelClass = "loglevel-error"; break;
-		case LogLevel.Warning: logLevelClass = "loglevel-warning"; break;
-		case LogLevel.Information: logLevelClass = "loglevel-information"; break;
-		case LogLevel.Debug: logLevelClass = "loglevel-debug"; break;
-		case LogLevel.Trace: logLevelClass = "loglevel-trace"; break;
-		case LogLevel.None: logLevelClass = ""; break;
-		default: throw new InvalidOperationError(`Unexpected log level "${args.logLevel}"`);
-	}
-
 	let logLevelName: string;
 	switch (args.logLevel)
 	{
-		case LogLevel.Critical: logLevelName = "Critical"; break;
-		case LogLevel.Error: logLevelName = "Error"; break;
-		case LogLevel.Warning: logLevelName = "Warning"; break;
-		case LogLevel.Information: logLevelName = ""; break;
-		case LogLevel.Debug: logLevelName = "Debug"; break;
-		case LogLevel.Trace: logLevelName = "Trace"; break;
-		case LogLevel.None: logLevelName = ""; break;
+		case LogLevel.Critical:
+			logLevelClass = "loglevel-critical";
+			logLevelName = "Critical";
+			break;
+		case LogLevel.Error:
+			logLevelClass = "loglevel-error";
+			logLevelName = "Error";
+			break;
+		case LogLevel.Warning:
+			logLevelClass = "loglevel-warning";
+			logLevelName = "Warning";
+			break;
+		case LogLevel.Information:
+			logLevelClass = "loglevel-information";
+			logLevelName = "";
+			break;
+		case LogLevel.Debug:
+			logLevelClass = "loglevel-debug";
+			logLevelName = "Debug";
+			break;
+		case LogLevel.Trace:
+			logLevelClass = "loglevel-trace";
+			logLevelName = "Trace";
+			break;
 		default: throw new InvalidOperationError(`Unexpected log level "${args.logLevel}"`);
 	}
 

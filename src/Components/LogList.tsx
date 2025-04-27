976ebcc5e95a0352eae0afed480631b31a72f654
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-27 06:45:20 -0300

    rewrite LogList to accept Log and AulaMessageLog

--- src/Components/LogList.tsx
+++ src/Components/LogList.tsx
@@ -4,6 +4,9 @@ import { LocalStorageFacade } from "../Services/LocalStorageFacade";
 import { loggers } from "../Services/loggers";
 import { ILogger, LogLevel } from "../Common/Logging";
 import Log from "./Log.tsx";
+import { IGatewayClientEvents, Message } from "aula.js";
+import { aulaClient } from "../Services/aulaClient.ts";
+import AulaMessageLog from "./AulaMessageLog.tsx";
 
 export default function LogList(args: LogListArgs)
 {
@@ -12,16 +15,33 @@ export default function LogList(args: LogListArgs)
 	useEffect(() =>
 	{
 		const logger = {
-			log: (level, message, id) =>
+			log: (level, message) =>
 			{
 				const selectedLogLevel = LocalStorageFacade.logLevel ?? LogLevel.Information;
 				if (level < selectedLogLevel)
 					return;
 
-				setLogs(previousLogs => [...previousLogs, { level, message, id: id ?? `l${previousLogs.length + 1}` }]);
+				setLogs(previousLogs => [...previousLogs, {
+					type: LogDataType.Console,
+					level,
+					message,
+					id: `l${previousLogs.length + 1}`
+				}]);
 			}
 		} satisfies ILogger;
 		loggers.add(logger);
+
+		const aulaMessageReceiver: IGatewayClientEvents["MessageCreated"] = (event) =>
+			setLogs(prev => [...prev, {
+				type: LogDataType.AulaMessage,
+				level: LogLevel.Information,
+				message: event.message,
+				id: event.message.id
+			}]);
+		aulaClient.on("MessageCreated", aulaMessageReceiver);
+		const aulaMessageRemover: IGatewayClientEvents["MessageRemoved"] = (event) =>
+			setLogs(prev => prev.toSpliced(prev.findIndex(v => v.id === event.messageId), 1));
+		aulaClient.on("MessageRemoved", aulaMessageRemover);
 
 		const logCleaner = () => setLogs([]);
 		events.on("LogClearRequest", logCleaner);
@@ -30,12 +50,20 @@ export default function LogList(args: LogListArgs)
 		{
 			loggers.remove(logger);
 			events.remove("LogClearRequest", logCleaner);
+			aulaClient.remove("MessageCreated", aulaMessageReceiver);
+			aulaClient.remove("MessageRemoved", aulaMessageRemover);
 		}
 	}, []);
 
 	return <div className="loglist">
 		{args.children}
-		{logs.map(l => <Log key={l.id} logLevel={l.level} message={l.message}/>)}
+		{logs.map(log =>
+		{
+			if (log.type === LogDataType.Console)
+				return <Log key={log.id} logLevel={log.level} message={log.message}/>;
+			if (log.type === LogDataType.AulaMessage)
+				return <AulaMessageLog props={{ message: log.message }}></AulaMessageLog>;
+		})}
 	</div>
 }
 
@@ -44,9 +72,26 @@ export interface LogListArgs
 	children?: ReactNode
 }
 
-interface LogData
+type LogData = ConsoleLogData | AulaMessageLogData;
+
+interface ConsoleLogData
 {
+	type: LogDataType.Console;
 	id: string;
 	level: LogLevel;
 	message: string;
+}
+
+interface AulaMessageLogData
+{
+	type: LogDataType.AulaMessage;
+	id: string;
+	level: LogLevel.Information;
+	message: Message;
+}
+
+enum LogDataType
+{
+	Console,
+	AulaMessage
 }

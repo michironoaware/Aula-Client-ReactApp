27ba9d650b8d7312372ef6ec59885d0c420f9982
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-28 12:07:47 -0300

    Create RestHelper.ts

---
+++ src/Services/Commands/RestHelper.ts
@@ -0,0 +1,40 @@
+﻿import { Func } from "../../Common";
+import { AulaRestError, RestClientNullAddressError } from "aula.js";
+import { loggers } from "../loggers.ts";
+import { LogLevel } from "../../Common/Logging";
+
+export namespace RestHelper
+{
+	export async function HandleRestErrors<T>(callback: Func<[], T>)
+	{
+		try
+		{
+			return {
+				succeeded: true,
+				value: await callback()
+			};
+		}
+		catch (err)
+		{
+			if (err instanceof AulaRestError)
+				loggers.log(LogLevel.Error, err.problemDetails?.detail ?? err.message);
+			else if (err instanceof RestClientNullAddressError)
+				loggers.log(LogLevel.Error, `A server address is required first. Execute "set-address" to set the server-address.`);
+			else
+			{
+				loggers.log(LogLevel.Critical, `An unexpected error occurred. ${(err as Error).message} ${(err as Error).stack}`);
+				if (err instanceof TypeError)
+					loggers.log(LogLevel.Warning,
+						"A problem occurred with the request; please verify that the address is valid or that the server is online.");
+			}
+
+			return { succeeded: false };
+		}
+	}
+}
+
+interface HandleRestErrorsResult<T>
+{
+	succeeded: boolean;
+	value?: T;
+}

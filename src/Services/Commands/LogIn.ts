2a1759b065f66eada93016502ea4f458f90fd00e
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-28 12:29:18 -0300

    Leverage code with RestHelper

--- src/Services/Commands/LogIn.ts
+++ src/Services/Commands/LogIn.ts
@@ -3,6 +3,7 @@ import { AulaRestError, CancellationToken, LogInRequestBody, LogInResponse, Rest
 import { gatewayClient, setToken } from "../gatewayClient.ts";
 import { loggers } from "../loggers";
 import { LogLevel } from "../../Common/Logging";
+import { RestHelper } from "./RestHelper.ts";
 
 export class LogIn extends Command
 {
@@ -44,32 +45,11 @@ export class LogIn extends Command
 			.withUserName(args.get(LogIn.#s_usernameOption.name)!)
 			.withPassword(args.get(LogIn.#s_passwordOption.name)!);
 
-		let logInResponse: LogInResponse;
-		try
-		{
-			logInResponse = await gatewayClient.rest.logIn(logInRequestBody, cancellationToken);
-		}
-		catch (err)
-		{
-
-			if (err instanceof AulaRestError)
-				loggers.log(LogLevel.Error, err.problemDetails?.detail ?? err.message);
-			else if (err instanceof RestClientNullAddressError)
-				loggers.log(LogLevel.Error, `A server address is required first. Execute "set-address" to set the server-address.`);
-			else
-			{
-				loggers.log(LogLevel.Critical, `An unexpected error occurred. ${(err as Error).message} ${(err as Error).stack}`);
-				if (err instanceof TypeError)
-					loggers.log(LogLevel.Warning,
-						"A problem occurred with the request; please verify that the address is valid or that the server is online.");
-			}
-			if (!(err instanceof Error))
-			{
-				throw err;
-			}
-
+		let logInAttempt = await RestHelper.HandleRestErrors(
+			async () => await gatewayClient.rest.logIn(logInRequestBody));
+		if (!logInAttempt.succeeded)
 			return;
-		}
+		let logInResponse = logInAttempt.value;
 
 		setToken(logInResponse.token);
 

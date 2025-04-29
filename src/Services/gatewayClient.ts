cfebde85967d556f766644c3e0a50f76239476cf
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-29 07:41:19 -0300

    configure gatewayClient to cache entities

--- src/Services/gatewayClient.ts
+++ src/Services/gatewayClient.ts
@@ -1,8 +1,10 @@
-﻿import { GatewayClient, GatewayClientOptions, Intents } from "aula.js";
+﻿import { GatewayClient, GatewayClientOptions, Intents, LruCache, RestClientOptions } from "aula.js";
 import { LocalStorageFacade } from "./LocalStorageFacade";
 
 export const gatewayClient = new GatewayClient(new GatewayClientOptions()
-	.withIntents(Intents.Users | Intents.Rooms | Intents.Messages | Intents.Moderation));
+	.withIntents(Intents.Users | Intents.Rooms | Intents.Messages | Intents.Moderation)
+	.withRestClientOptions(new RestClientOptions()
+		.withCache(new LruCache(256, false))));
 
 const storedAddress = LocalStorageFacade.serverAddress;
 if (storedAddress !== null)

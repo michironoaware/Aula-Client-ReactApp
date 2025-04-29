be4eca32a6c11e0394d156b119ddabcbd343b8c6
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-29 15:23:25 -0300

    move LogCollection to logging service file

--- src/Services/logging.ts
+++ src/Services/logging.ts
@@ -1,3 +1,31 @@
-﻿import { LogCollection } from "../Collections";
+﻿import { ILogger, LogLevel } from "../Common/Logging";
+
+class LogCollection implements ILogger
+{
+	readonly #_loggers: ILogger[] = [];
+
+	public add(logger: ILogger)
+	{
+		this.#_loggers.push(logger);
+	}
+
+	public remove(logger: ILogger)
+	{
+		this.#_loggers.splice(this.#_loggers.indexOf(logger), 1);
+	}
+
+	public clear()
+	{
+		this.#_loggers.length = 0;
+	}
+
+	public log(logLevel: LogLevel, message: string)
+	{
+		for (const logger of this.#_loggers)
+		{
+			logger.log(logLevel, message);
+		}
+	}
+}
 
 export const logging = new LogCollection();

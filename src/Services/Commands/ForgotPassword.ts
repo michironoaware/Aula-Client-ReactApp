e75a5a39209502a9a85c552d54164b5ff5b06097
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-29 08:41:57 -0300

    Create ForgotPassword.ts

---
+++ src/Services/Commands/ForgotPassword.ts
@@ -0,0 +1,47 @@
+﻿import { Command, CommandOption } from "../../Commands";
+import { CancellationToken, ForgotPasswordQuery } from "aula.js";
+import { WebEncoders } from "../../Common/WebEncoders.ts";
+import { RestHelper } from "./RestHelper.ts";
+import { gatewayClient } from "../gatewayClient.ts";
+import { loggers } from "../loggers.ts";
+import { LogLevel } from "../../Common/Logging";
+
+export class ForgotPassword extends Command
+{
+	static readonly #s_emailOption = new CommandOption({
+		name: "e",
+		description: "The email of the account to reset the password to.",
+		isRequired: true,
+		requiresArgument: true,
+		canOverflow: false,
+	});
+
+	public constructor()
+	{
+		super();
+		this.addOption(ForgotPassword.#s_emailOption);
+	}
+
+	public get name()
+	{
+		return "forgot-password";
+	}
+
+	public get description()
+	{
+		return "Sends a password reset email with a code to reset an account's password.";
+	}
+
+	public async callback(args: Readonly<Map<string, string>>, cancellationToken: CancellationToken): Promise<void>
+	{
+		const email = WebEncoders.ToBase64UrlString(args.get(ForgotPassword.#s_emailOption.name)!);
+
+		const sendEmailAttempt = await RestHelper.HandleRestErrors(async () =>
+			await gatewayClient.rest.forgotPassword(new ForgotPasswordQuery().withEmail(email), cancellationToken));
+		if (!sendEmailAttempt.succeeded)
+			return;
+
+		loggers.log(LogLevel.Information,
+			"Password reset email request sent. If an account with the provided email exists, an email will be sent.");
+	}
+}

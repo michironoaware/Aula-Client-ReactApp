e3967eac78ed797108f8828446c8c42242eb060b
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-29 07:37:27 -0300

    Create ConfirmEmail.ts

---
+++ src/Services/Commands/ConfirmEmail.ts
@@ -0,0 +1,92 @@
+﻿import { Command, CommandOption } from "../../Commands";
+import { CancellationToken, ConfirmEmailQuery } from "aula.js";
+import { RestHelper } from "./RestHelper.ts";
+import { gatewayClient } from "../gatewayClient.ts";
+import { loggers } from "../loggers.ts";
+import { LogLevel } from "../../Common/Logging";
+import { WebEncoders } from "../../Common/WebEncoders.ts";
+
+export class ConfirmEmail extends Command
+{
+	static readonly #s_emailOption = new CommandOption({
+		name: "e",
+		description: "The email to confirm.",
+		isRequired: true,
+		requiresArgument: true,
+		canOverflow: false,
+	});
+
+	static readonly #s_sendEConfirmationEmailOption = new CommandOption({
+		name: "send-confirmation-email",
+		description: "Sends a confirmation email to the specified email address. " +
+		             "If a confirmation token is specified, this argument will be ignored and no email will be sent.",
+		isRequired: false,
+		requiresArgument: false,
+		canOverflow: false,
+	});
+
+	static readonly #s_confirmationTokenOption = new CommandOption({
+		name: "t",
+		description: "The token sent in the confirmation email used to confirm the specified email address.",
+		isRequired: false,
+		requiresArgument: true,
+		canOverflow: false,
+	});
+
+	public constructor()
+	{
+		super();
+		this.addOption(ConfirmEmail.#s_emailOption);
+		this.addOption(ConfirmEmail.#s_confirmationTokenOption);
+		this.addOption(ConfirmEmail.#s_sendEConfirmationEmailOption);
+	}
+
+	public get name()
+	{
+		return "confirm-email";
+	}
+
+	public get description()
+	{
+		return "Provides options to confirm an email address.";
+	}
+
+	public async callback(args: Readonly<Map<string, string>>, cancellationToken: CancellationToken): Promise<void>
+	{
+		let email = args.get(ConfirmEmail.#s_emailOption.name)!;
+		let confirmationToken = args.get(ConfirmEmail.#s_confirmationTokenOption.name);
+		const sendConfirmationEmailFlag = args.get(ConfirmEmail.#s_sendEConfirmationEmailOption.name);
+		email = WebEncoders.ToBase64UrlString(email);
+		confirmationToken = confirmationToken ? WebEncoders.ToBase64UrlString(confirmationToken) : confirmationToken;
+
+		if (confirmationToken === undefined &&
+		    sendConfirmationEmailFlag === undefined)
+		{
+			loggers.log(LogLevel.Warning,
+				`No action was selected. Please specify either ` +
+				`"${CommandOption.prefix}${ConfirmEmail.#s_confirmationTokenOption.name}" (to confirm with a token) ` +
+				`or "${CommandOption.prefix}${ConfirmEmail.#s_sendEConfirmationEmailOption.name}" (to send a confirmation email).`
+			);
+			return;
+		}
+
+		if (confirmationToken !== undefined)
+		{
+			const confirmEmailQuery = new ConfirmEmailQuery()
+				.withEmail(email)
+				.withToken(confirmationToken);
+
+			await RestHelper.HandleRestErrors(async () => await gatewayClient.rest.confirmEmail(confirmEmailQuery));
+			return;
+		}
+
+		const confirmEmailQuery = new ConfirmEmailQuery().withEmail(email);
+		const emailSending = await RestHelper.HandleRestErrors(
+			async () => await gatewayClient.rest.confirmEmail(confirmEmailQuery));
+		if (!emailSending.succeeded)
+			return;
+
+		loggers.log(LogLevel.Information,
+			"Confirmation email request sent. If an account with the provided email exists, a confirmation email will be sent.");
+	}
+}

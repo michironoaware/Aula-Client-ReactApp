81ae2397f5b5a804c2dc7a419cc77847e687744c
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-04-29 14:55:33 -0300

    Create LogOut.ts

---
+++ src/Services/Commands/LogOut.ts
@@ -0,0 +1,25 @@
+﻿import { Command } from "../../Commands";
+import { CancellationToken } from "aula.js";
+import { aula, AulaConnectionState } from "../aula.ts";
+
+export class LogOut extends Command
+{
+	public get name()
+	{
+		return "logout";
+	}
+
+	public get description()
+	{
+		return "Sign out of your account on this device.";
+	}
+
+	public async callback(args: Readonly<Map<string, string>>, cancellationToken: CancellationToken): Promise<void>
+	{
+		if (aula.connectionState !== AulaConnectionState.Disconnected)
+			await aula.disconnect();
+
+		aula.logOutLocally();
+		window.location.reload();
+	}
+}

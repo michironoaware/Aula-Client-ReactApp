3dcc933990ea114b5e3158457aafcae604fa4c2f
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-05-05 17:53:14 -0300

    create main App component

---
+++ src/components/App.tsx
@@ -0,0 +1,21 @@
+﻿import React from "react";
+import Theme from "./Theme.tsx";
+import Body from "./Body.tsx";
+import LogList from "features/logs/components/LogList.tsx";
+import InputTextBox from "features/commands/components/InputTextBox.tsx";
+import { logging } from "lib/logging.ts";
+import { LogLevel } from "utils/logging/LogLevel.ts";
+
+export default function App()
+{
+	logging.add({
+		log: (logLevel, message) => console.log(`[${LogLevel[logLevel]}]: ${message}`),
+	});
+
+	return <Theme withEffects={true}>
+		<Body>
+			<LogList></LogList>
+			<InputTextBox></InputTextBox>
+		</Body>
+	</Theme>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,12 @@
-import { logging } from "lib/logging.ts";
-import { LogLevel } from "utils/logging/LogLevel.ts";
 import { createRoot } from "react-dom/client";
-import React, { StrictMode } from "react";
-import Body from "./components/Body.tsx";
-import InputTextBox from "./features/commands/components/InputTextBox.tsx";
-import Theme from "./components/Theme.tsx";
-import LogList from "./features/logs/components/LogList.tsx";
+import App from "components/App.tsx";
+import { StrictMode } from "react";
 
-logging.add({
-	log: (logLevel, message) => console.log(`[${LogLevel[logLevel]}]: ${message}`),
-});
 
 const root = createRoot(document.getElementById("root")!);
 
 root.render(
 	<StrictMode>
-		<Theme withEffects={true}>
-			<Body>
-				<LogList></LogList>
-				<InputTextBox></InputTextBox>
-			</Body>
-		</Theme>
+		<App></App>
 	</StrictMode>
 );

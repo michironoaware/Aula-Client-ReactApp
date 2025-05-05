2fcedd78fd3bf76bd0d8c0f3172321031d179f72
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-05-05 19:17:15 -0300

    Create Say.ts

---
+++ src/features/commands/services/commands/Say.ts
@@ -0,0 +1,60 @@
+﻿import { Command } from "lib/commands/Command.ts";
+import { CancellationToken, MessageFlags, MessageType, SendMessageRequestBody } from "aula.js";
+import { CommandOption } from "lib/commands/CommandOption.ts";
+import { aula } from "lib/aula.ts";
+import { logging } from "lib/logging.ts";
+import { LogLevel } from "utils/logging/LogLevel.ts";
+
+export class Say extends Command
+{
+	static readonly #s_messageOption = new CommandOption({
+		name: "m",
+		description: "The message you want to speak aloud to others nearby.",
+		isRequired: true,
+		requiresArgument: true,
+		canOverflow: true,
+	});
+
+	static readonly #s_hideAuthorOption = new CommandOption({
+		name: "h",
+		description: "Indicates the message should not show the author.",
+		isRequired: false,
+		requiresArgument: false,
+		canOverflow: false,
+	});
+
+	public constructor()
+	{
+		super();
+		this.addOption(Say.#s_messageOption);
+		this.addOption(Say.#s_hideAuthorOption);
+	}
+
+	public get name()
+	{
+		return "say";
+	}
+
+	public get description()
+	{
+		return "Say something aloud to nearby presences."
+	}
+
+	public async callback(args: Readonly<Map<string, string>>, cancellationToken: CancellationToken): Promise<void>
+	{
+		const message = args.get(Say.#s_messageOption.name)!;
+		const hideAuthor = !!args.get(Say.#s_hideAuthorOption.name);
+
+		if (!aula.gateway.currentUser?.currentRoomId)
+		{
+			logging.log(LogLevel.Error,
+				"Your attempt to speak has been lost to the void—no room exists to give meaning to your words.");
+			return;
+		}
+
+		await aula.rest.sendMessage(aula.gateway.currentUser.currentRoomId, new SendMessageRequestBody()
+			.withType(MessageType.Standard)
+			.withText(message)
+			.withFlags(hideAuthor ? MessageFlags.HideAuthor : null), cancellationToken);
+	}
+}

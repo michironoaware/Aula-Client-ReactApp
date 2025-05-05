7f362b3c55f828a492ce94d327cce2e18e70babe
rexaliis <77251557+michironoaware@users.noreply.github.com>
2025-05-05 13:08:58 -0300

    migrate to vite

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+﻿import { defineConfig } from 'vite'
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgr from "vite-plugin-svgr";
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+	publicDir: 'public',
+	plugins: [tsconfigPaths(), svgr(), react()],
+	base: '/app',
+});
